2c65ff89c691d010622f2b69df8a699565197f3c
daiwber <1846327762@qq.com>
2025-06-18 09:35:56 +0800

    Update environment_understand.py

--- 机器人端/传输与调用/environment_understand.py
+++ 机器人端/传输与调用/environment_understand.py
@@ -107,7 +107,7 @@ def file_to_base64(file_path):
 
 
 base_url = 'https://api.mindcraft.com.cn/v1/'
-api_key = 'MC-FC7B52EE000042F39B1679233E2AC396'
+api_key = 'MC-FC7B52EE000042Fxxxxx'
 
 client = OpenAI(base_url=base_url, api_key=api_key)
 
